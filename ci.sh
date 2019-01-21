b5013a4fbce164e068b459c12d81ab1300db1225
Magnus Boye <magnusboye@gmail.com>
2019-01-21 22:36:05 +0200

    Suppress apt-get update output

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-sudo apt-get update
-sudo apt-get -y install python3 python3-pip
+sudo apt-get update &>/dev/null
+sudo apt-get -y install python3 python3-pip &>/dev/null
 sudo -H pip3 install robotframework
 python --version
 go get ./...
